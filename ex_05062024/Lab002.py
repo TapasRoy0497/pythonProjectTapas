dea7e774777c1f4c5267fa73edf406cbc2ced357
TapasRoy0497 <tapasroy0497@gmail.com>
2024-06-05 08:22:02 +0530

    Day 2 (05/06/2024)

---
+++ ex_05062024/Lab002.py
@@ -0,0 +1,4 @@
+print("Hello", "World", "123", True, 3.14)
+print("Hello", "World", "123", True, 3.14, sep="-")
+print("Hello", "World", "123", True, 3.14, sep="-", end=" && ")
+print("Hello", "World", "123", True, 3.14, sep="-")
---
+++ ex_05062024/Lab003.py
@@ -0,0 +1,3 @@
+# Program to find max of two numbers
+
+print(max(78, 345))
