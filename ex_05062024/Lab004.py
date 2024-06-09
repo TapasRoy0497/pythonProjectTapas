d79fa3b5f88730d376d72bfcb053e87d8e106ee6
TapasRoy0497 <tapasroy0497@gmail.com>
2024-06-10 01:47:17 +0530

    Day 3 (07/06/2024)

--- ex_05062024/Lab004.py
+++ ex_05062024/Lab004.py
@@ -1,5 +1,13 @@
 # Taking input from user
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+print(first_name, last_name, age)
+print("Your first name is " + first_name)
+print("Your last name is " + last_name)
+print("Your age is " + age)
 
-print(first_name, last_name)
+# Printing the type of input
+print(type(first_name))
+print(type(last_name))
+print(type(age))
